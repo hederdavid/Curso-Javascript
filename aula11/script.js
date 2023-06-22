6124e003f09079a53cbf3a6196cd2ba87232411f
Heder M. David <116081626+hederdavid@users.noreply.github.com>
2023-06-22 18:57:42 -0300

    exercicios

---
+++ aula11/script.js
@@ -0,0 +1,7 @@
+/* window.alert("Mensagem")
+window.confirm("Mensagem")
+window.prompt("Digite seu nome") */
+
+const numero1 = parseFloat(prompt("Insira um número: "))
+const numero2 = parseFloat(prompt("Insira outro número: "))
+alert(`A soma entre o número ${numero1} e o número ${numero2} tem como resultado o número ${numero1+numero2}.`)
