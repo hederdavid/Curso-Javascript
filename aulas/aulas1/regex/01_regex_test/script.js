a9430e30e0ef642de7c70d6efd5043f31b8b4c34
Heder M. David <116081626+hederdavid@users.noreply.github.com>
2023-07-17 21:50:21 -0300

    aulas

---
+++ aulas/aulas1/regex/01_regex_test/script.js
@@ -0,0 +1,5 @@
+const reg1 = new RegExp("bola")
+console.log(reg1.test("Tem bola?"))
+console.log(reg1.test("Não tem"))
+
+console.log(/quadrado/.test("4156456465quadrado456.489489"))
---
+++ aulas/aulas1/regex/02_conjuntos/script.js
@@ -0,0 +1,3 @@
+const reg1 = /[0-9]/ // verifica se contém números.
+
+console.log(reg1.test("486748946186456486"))
